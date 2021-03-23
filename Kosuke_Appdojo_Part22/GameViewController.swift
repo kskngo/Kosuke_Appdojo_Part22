e2c589c017b63a3503e490ad0c08390f58dd85f2
Kosuke Nagao <kosukengo@gmail.com>
2021-03-23 20:33:29 +0900

    テンプレートから不要な処理を削除と画像追加

--- Kosuke_Appdojo_Part22/GameViewController.swift
+++ Kosuke_Appdojo_Part22/GameViewController.swift
@@ -10,25 +10,13 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+
+        let scene = GameScene(size: CGSize(width: 750, height: 1334))
+        guard let skView = self.view as? SKView else { return }
+        scene.scaleMode = .aspectFit
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Kosuke_Appdojo_Part22
+## 課題動画
+[[Xcodeアプリ開発入門][課題] Part22 SpriteKitでサイコロを表示する](https://www.youtube.com/watch?v=ZKLyT0navNQ&list=PLQ5rERkGSxF-fsdBNQu70r5r0OPwSVygO&index=33)
+## 参考動画
+[]()
